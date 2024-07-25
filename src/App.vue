d41825aeab6fb917739150f98a3e9e3bf5f1882f
kite <2257440643@qq.com>
2024-07-25 22:55:05 +0800

    feat: add button component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,43 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Button from '@/components/Button/Button.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>s-element</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <h2>Button</h2>
+    <Button ref="buttonRef">Test Button</Button>
+    <Button plain>Plain Button</Button>
+    <Button round>Round Button</Button>
+    <Button circle>VK</Button>
+    <Button disabled>Disabled Button</Button><br /><br />
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button><br /><br />
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button><br /><br />
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button><br /><br />
+    <Button size="large" loading>Loading</Button>
+    <Button size="large" icon="arrow-up">Icon</Button><br /><br />
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 600;
 }
 
 .logo {
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    ref="instance"
+    class="sk-button"
+    :class="{
+      [`sk-button--${type}`]: type,
+      [`sk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading
+    }"
+    :disabled="disabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+defineOptions({
+  name: 'SkButton'
+})
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button'
+})
+
+const instance = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: instance
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
